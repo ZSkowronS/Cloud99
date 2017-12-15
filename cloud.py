624509ca6b877381abd47591b04bedaf22e786c9
Zbigniew Skowron <z.skowron@samsung.com>
2017-12-15 14:00:25 +0100

    Adding wait_for_attach() call at he beginning of the program.

--- cloud.py
+++ cloud.py
@@ -1,5 +1,13 @@
+from __future__ import print_function
 import ptvsd
+
+print("Before attach.")
+
 ptvsd.enable_attach(secret = 'zaq12WSX', address = ('0.0.0.0', 8080))
+
+print("Waiting to attach.")
+ptvsd.wait_for_attach()
+print("Attached.")
 
 import sys
 from cloud99.cli.commands import cli
